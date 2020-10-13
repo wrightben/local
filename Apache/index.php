e97b6c26797f16c5fb02865eb1d36e115e62c9ce
wrightben <mail@wrightben.com>
2020-10-13 04:59:33 -0400

    gitall commit

---
+++ Apache/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>HTML</title>
+	</head>
+	<body>
+		This is an HTML document in /Library/Webserver/Documents
+	</body>
+</html>
+<?php
+	echo "PHP enabled.";
+?>
