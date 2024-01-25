7aebbabd5524d19c97d3c528b6dec34915463d49
Benjamin Wright <mail@wrightben.com>
2024-01-25 06:03:00 -0500

    gitall commit

--- core.sh
+++ core.sh
@@ -1,39 +1,12 @@
-#export JAVA_HOME=$(/usr/libexec/java_home) # Possible to grep here.
-thisuser=$(whoami)
-export BIN_HOME="~/Desktop/Local/bin/"
-export PATH=$BIN_HOME:$PATH:$JAVA_HOME/bin
-export DEPLOY="/Users/$thisuser/Desktop/Local/project-deploy/"
-
-
 # =============
 # Includes
 # =============
 . ~/Desktop/local/git/git.sh
 
 
-
-# =============
-# MySQL
-# 
-# Turn it on
-#
-# cd /usr/local/mysql; sudo ./bin/mysqld_safe
-#
-# =============
-alias mysql=/usr/local/mysql/bin/mysql
-alias mysqladmin=/usr/local/mysql/bin/mysqladmin
-
-
-
-
 # =============
 # Locations
 # =============
-w() {
-	cd /Library/WebServer/Documents/
-}
-export w
-
 d() {
 	cd ~/Desktop
 }
